b9558a92faa30b4cb8e4ea89841742a9ce85242f
Harshalmohite12 <harshalmohite35100@gmail.com>
2025-09-08 23:36:01 +0530

    .

--- FileIO.py
+++ FileIO.py
@@ -96,22 +96,99 @@
 
 import csv
 
-import csv
+# with open("students.csv", "r") as file:
+#     reader = csv.reader(file)
+#     for row in reader:
+#         print(row)
 
-# Step 2a: Open a file to write
-with open("students.csv", "w", newline="") as file:
-    # Step 2b: Create a writer object
-    writer = csv.writer(file)
+# student_count = 0
+# with open("students.csv", "r") as file:
+#     read = csv.reader(file)
+#     header = next(read)
+#     print("Header is : ", header)
+#     for row in read:
+#         print(row)
+        # student_count += 1
+    # print(student_count)
     
-    # Step 2c: Write the header row
-    writer.writerow(["Name", "Math", "Science", "English"])
+# with open("students.csv", "w", newline = "") as file:
+#     write = csv.writer(file)
+#     write.writerow(["Name", "Math", "Science", "English"])
     
-    # Step 2d: Write data rows
-    writer.writerow(["Harshal", 90, 85, 92])
-    writer.writerow(["Anita", 78, 88, 80])
+#     write.writerow(["Harshal", "89", "86", "98"])
+#     write.writerow(["Shweta", "89", "86", "98"])
+#     write.writerow(["Chaitanya", "89", "86", "98"])
+    
 
 
-with open("students.csv", "r") as file:
-    reader = csv.reader(file)
-    for row in reader:
-        print(row)
+# rows = [
+#     ["Name", "Math", "Science", "English"],
+#     ["Harshal", 90, 85, 92],
+#     ["Anita", 78, 88, 80],
+#     ["Rohit", 85, 80, 90]
+# ]
+
+# with open("students.csv", "w", newline="") as file:
+#     writer = csv.writer(file)
+#     writer.writerows(rows)   # writes all rows in one go
+
+
+# with open("books.csv", "w", newline="") as file:
+#     writer = csv.writer(file)
+    
+#     writer.writerow(["Title", "Author", "Year"])
+
+#     writer.writerow(["Physics", "H.C. VERMA", "2005"])
+#     writer.writerow(["Chemistry", "Pankaj Sir PW", "2008"])
+#     writer.writerow(["Maths", "Alakh Sir", "2012"])
+
+# rows = [
+#     ["name", "director", "rating"],
+#     ["Singham", "rohit shetty","4.5"],
+#     ["Singham2", "rohit shetty", "4.8"],
+#     ["Singham3", "Rohit Shetty", "9.8"]
+# ]
+
+# with open("movies.csv", "w", newline="") as file:
+#     writer = csv.writer(file)
+#     writer.writerows(rows)
+    
+# with open("movies.csv", "a", newline="") as file:
+#     writer = csv.writer(file)
+#     writer.writerow(["Singham4", "rohit shetty", "8.8"])
+
+# data = [
+#     ["Players", "Team", "Runs"],
+#     ["Virat", "RCB", "973"],
+#     ["Rohit", "MI", "865"],
+#     ["Shubhman", "GT", "890"]
+# ]
+# with open("players.csv", "w", newline="") as file:
+#     writer = csv.writer(file)
+    
+#     writer.writerows(data)
+    
+# with open("players.csv", "r") as file:
+#     reader = csv.reader(file)
+#     next(reader)
+#     for read in (reader):
+#         print(read[0])
+    
+# with open("players.csv", "a", newline="") as file:
+#     writer = csv.writer(file)
+#     writer.writerow(["gamhir", "KKR", "780"])
+    
+# with open("players.csv", "r") as file:
+#     reader = csv.reader(file)
+#     next(reader)
+#     for read in (reader):
+#         print(read[0],"->",read[2])
+        
+# total_runs = 0
+# with open("players.csv", "r") as file:
+#     reader = csv.reader(file)
+#     next(reader)
+#     for read in (reader):
+#         runs = int(read[2])
+#         total_runs += runs
+#     print(total_runs)
